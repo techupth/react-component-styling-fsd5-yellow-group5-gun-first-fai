c493f20c9aca0e85faf33aa4b07e61abc012aec0
datfoi <jengjiening@gmail.com>
2023-12-20 13:32:43 +0700

    feat: assignment-component-styling/fai

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 import "./App.css";
+import Button from "./components/Button";
+import Alert from "./components/Alert";
 
 function App() {
   return (
-    <div className="App">
-      <div className="button-components-section">
-        {/* Render ตัว Button 2 แบบ */}
+    <>
+      <div className="App">
+        <div className="button-components-section">
+          {/* Render ตัว Button 2 แบบ */}
+          <Button type="primary" />
+          <br></br>
+          <Button type="secondary" />
+        </div>
+        <hr />
+        <div className="alert-components-section">
+          {/* Render ตัว Alert 4 แบบ */}
+          <Alert type="error" />
+          <br></br>
+          <Alert type="warning" />
+          <br></br>
+          <Alert type="info" />
+          <br></br>
+          <Alert type="success" />
+        </div>
       </div>
-      <hr />
-      <div className="alert-components-section">
-        {/* Render ตัว Alert 4 แบบ */}
-      </div>
-    </div>
+    </>
   );
 }
 
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,69 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function Button() {}
+function Button(props) {
+  if (props.type === "primary") {
+    return (
+      <div
+        css={css`
+          display: flex;
+          width: 170px;
+          height: 50px;
+          padding: 0px 16px;
+          justify-content: center;
+          align-items: center;
+          gap: 8px;
+          flex-shrink: 0;
+          border-radius: 4px;
+          background: #074ee8;
+        `}
+      >
+        <h1
+          css={css`
+            color: #fff;
+            font-family: Sarabun;
+            font-size: 16px;
+            font-style: normal;
+            font-weight: 400;
+            line-height: 18px;
+          `}
+        >
+          Large
+        </h1>
+      </div>
+    );
+  } else if (props.type === "secondary") {
+    return (
+      <div
+        css={css`
+          display: flex;
+          width: 171.19px;
+          height: 50px;
+          padding: 0px 16px;
+          justify-content: center;
+          align-items: center;
+          gap: 8px;
+          flex-shrink: 0;
+          border-radius: 4px;
+          background: #07a4e8;
+        `}
+      >
+        <h1
+          css={css`
+            color: var(--Surface, #fff);
+            font-family: Sarabun;
+            font-size: 16px;
+            font-style: normal;
+            font-weight: 400;
+            line-height: 18px; /* 112.5% */
+          `}
+        >
+          Large
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Button;
