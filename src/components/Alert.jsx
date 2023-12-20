2498651a5fe57a5e9ba42c083f024144c3ff310d
Gun <cobaltgun2324@gmail.com>
2023-12-20 14:26:50 +0700

    feat: complete assignment

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,3 +1,115 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function Alert() {}
+function Alert(props) {
+  if (props.type === "error") {
+    return (
+      <div
+        className="box"
+        css={css`
+          border-radius: 10px;
+          background: #f9c8c8;
+        `}
+      >
+        <div
+          className="textInBox"
+          css={css`
+            width: 545px;
+            height: 33px;
+            color: #444;
+            font-family: Kanit;
+            font-size: 20px;
+            font-style: normal;
+            font-weight: 700;
+            line-height: normal;
+          `}
+        >
+          <img src="src/images/frown.jpg" /> This is error alert box
+        </div>
+      </div>
+    );
+  } else if (props.type === "warning") {
+    return (
+      <div
+        className="box2"
+        css={css`
+          border-radius: 10px;
+          background: #f9d9c8;
+        `}
+      >
+        <div
+          className="textInBox2"
+          css={css`
+            width: 545px;
+            height: 33px;
+            color: #444;
+            font-family: Kanit;
+            font-size: 20px;
+            font-style: normal;
+            font-weight: 700;
+            line-height: normal;
+          `}
+        >
+          <img src="src/images/alert-triangle.png" />
+          This is warning alert box
+        </div>
+      </div>
+    );
+  } else if (props.type === "info") {
+    return (
+      <div
+        className="box3"
+        css={css`
+          border-radius: 10px;
+          background: #f9ebc8;
+        `}
+      >
+        <div
+          className="textInBox3"
+          css={css`
+            width: 545px;
+            height: 33px;
+            color: #444;
+            font-family: Kanit;
+            font-size: 20px;
+            font-style: normal;
+            font-weight: 700;
+            line-height: normal;
+          `}
+        >
+          <img src="src/images/alert-circle.png" />
+          This is info alert box
+        </div>
+      </div>
+    );
+  } else if (props.type === "success") {
+    return (
+      <div
+        className="box4"
+        css={css`
+          border-radius: 10px;
+          background: #cef7cd;
+        `}
+      >
+        <div
+          className="textInBox4"
+          css={css`
+            width: 545px;
+            height: 33px;
+            color: #444;
+            font-family: Kanit;
+            font-size: 20px;
+            font-style: normal;
+            font-weight: 700;
+            line-height: normal;
+          `}
+        >
+          <img src="src/images/check-circle.png" />
+          This is success alert box
+        </div>
+      </div>
+    );
+  }
+}
+export default Alert;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,43 @@
 // Start coding here
-
-function Button() {}
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+function Button(props) {
+  if (props.type === "primary") {
+    return (
+      <div>
+        Primary
+        <button
+          css={css`
+            width: 171.19px;
+            height: 50px;
+            radius: 4px;
+            padding: 0px, 16px, 0px, 16px;
+            gap: 8px;
+            background-color: #074ee8;
+          `}
+        >
+          Large
+        </button>
+      </div>
+    );
+  } else if (props.type === "secondary") {
+    return (
+      <div>
+        Secondary
+        <button
+          css={css`
+            width: 171.19px;
+            height: 50px;
+            radius: 4px;
+            padding: 0px, 16px, 0px, 16px;
+            gap: 8px;
+            background-color: #07a4e8;
+          `}
+        >
+          Large
+        </button>
+      </div>
+    );
+  }
+}
+export default Button;
